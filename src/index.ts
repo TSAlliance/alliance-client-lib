81d6b16b11e5d6ffe99325b7132d910f36df15d0
Zitzmann <cedric.zitzmann@gmail.com>
2021-07-10 01:25:57 +0200

    added events to exports

--- src/index.ts
+++ src/index.ts
@@ -7,5 +7,21 @@ import { Pageable } from "./pagination/pageable";
 import { AllianceSDK } from "./request/request";
 import { HashMap } from "./util/hashMap";
 import { Response } from "./response/response";
+import { EventPublisher } from "./event/eventPublisher";
+import { Event } from "./event/event";
+import { EventListener } from "./event/eventListener";
 
-export { Page, Pageable, AllianceSDK, ApiError, HashMap, ErrorHandler, UserDetails, UserDetailsService, Response };
+export {
+    Page,
+    Pageable,
+    AllianceSDK,
+    ApiError,
+    HashMap,
+    ErrorHandler,
+    UserDetails,
+    UserDetailsService,
+    Response,
+    EventPublisher,
+    EventListener,
+    Event,
+};
