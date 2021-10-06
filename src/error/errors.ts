11361a81795d232ab838d97790c592961e77f1a6
Cedric Zitzmann <cedric.zitzmann@gmail.com>
2021-10-06 09:01:14 +0200

    upgraded to new apierror construction

--- src/error/errors.ts
+++ src/error/errors.ts
@@ -1,15 +1,28 @@
 import { ApiError } from "./apiError";
 
-export class ClientInternalError extends ApiError {
+export class InternalError extends ApiError {
     constructor() {
-        super("Ein interner App-Fehler ist aufgetreten.", "INTERNAL_ERROR", { statusCode: 500 });
+        super("Ein interner Server-Fehler ist aufgetreten.", "INTERNAL_ERROR", { 
+            statusCode: 500,
+            isCritical: true 
+        });
     }
 }
 
-export class ClientNetworkError extends ApiError {
+export class NetworkError extends ApiError {
     constructor() {
         super("Es ist ein Netzwerkfehler aufgetreten. Bitte überprüfe deine Internetverbindung", "NETWORK_ERROR", {
             statusCode: 500,
+            isCritical: true
+        });
+    }
+}
+
+export class ServiceUnavailableError extends ApiError {
+    constructor() {
+        super("Der Service ist derzeit nicht erreichbar", "SERVICE_UNAVAILABLE", {
+            statusCode: 500,
+            isCritical: true
         });
     }
 }
